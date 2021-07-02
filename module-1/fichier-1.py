1630d09a77b28eaec476de4b3a0db446a0221bf8
DavidAsad-Syed <86836734+DavidAsad-Syed@users.noreply.github.com>
2021-07-02 16:31:22 +0200

    cours 1 prog (révision)

---
+++ module-1/fichier-1.py
@@ -0,0 +1,69 @@
+a = 1
+b = 3
+if (a == 1) or (b == 3):
+    print('oui')
+else:
+    print('non')
+
+if (a == 1) and (b == 3):
+    print('oui')
+else:
+    print('non')
+
+
+def fa(x):
+    print(f'{x}+{x}-{x}')
+    a = input()
+    b = float(a)
+    '''
+    float converti une chaine de caractère en nombre a virgule
+    '''
+    c = str(b)
+    '''
+    inverse de float
+    '''
+
+    print(type(b))
+    if (b != x):
+        print(f'faux, la valeurs est pas égale {b}')
+    else: 
+        print('juste')
+
+fa(2)
+fa(1)
+x = [9, 4, 0, 5, 3]
+
+print(len(x))
+
+x.append(6)
+
+x.sort(reverse=True)
+print(x)
+
+
+
+
+
+
+
+
+print(x[5])
+
+a = 8
+b = 7
+c = a + b 
+
+d = a - b
+
+e = a / b
+
+f = a * b
+
+g = a % 2  # g = 0
+
+h = b % 2  # h = 1
+
+"""
+% -> divise sans les nombres à virgules et garde le surplus
+"""
+
---
+++ module-1/fichier-2.py
@@ -0,0 +1,60 @@
+a = 0.1
+b = 1 / 10
+
+if a <= b:
+    print('same')
+else:
+    print('different'
+
+
+
+
+
+a = 1
+b = 3
+if (a == 1) or (b == 3):
+    print('oui')
+else:
+    print('non')
+
+if (a == 1) and (b == 3):
+    print('oui')
+else:
+    print('non')
+
+print('1+1-1')
+a = input()
+b = float(a)
+c = str(b)
+print(type(b))
+if (b != 1):
+    print('faux')
+else: 
+    print('juste')
+
+
+
+
+
+
+
+
+
+a = 8
+b = 7
+c = a + b 
+
+d = a - b
+
+e = a / b
+
+f = a * b
+
+g = a % 2  # g = 0
+
+h = b % 2  # h = 1
+
+"""
+% -> divise sans les nombres à virgules et garde le surplus
+"""
+
